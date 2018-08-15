ebbd9e2ec59cbe889069740868f09e2ecbd7aa13
ArturAzarau <artur.azarau@gmail.com>
2018-08-15 17:26:07 +0300

    - UI added - Observation of keyboard events(will hide and change frame) and its handling added

--- Secret Swift/ViewController.swift
+++ Secret Swift/ViewController.swift
@@ -10,8 +10,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: - Properties
+    
+    @IBOutlet var secret: UITextView!
+    
+    // MARK: - Actions
+    
+    @IBAction func authenticateTapped(_ sender: Any) {
+    }
+    
+    // MARK: - View life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        registerForKeyboardNotifications()
+    }
+    
+    // MARK: - Methods
+    
+    private func registerForKeyboardNotifications() {
+        let notificationCenter = NotificationCenter.default
+        notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard(notification:)), name: Notification.Name.UIKeyboardWillHide, object: nil)
+        notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard(notification:)), name: Notification.Name.UIKeyboardWillChangeFrame, object: nil)
+    }
+    
+    @objc private func adjustForKeyboard(notification: Notification) {
+        let userInfo = notification.userInfo!
+        
+        let keyboardScreenEndFrame = (userInfo[UIKeyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
+        let keyboardViewEndFrame = view.convert(keyboardScreenEndFrame, to: view.window)
+        if notification.name == Notification.Name.UIKeyboardWillHide {
+            secret.contentInset = .zero
+        } else {
+            secret.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: keyboardViewEndFrame.height, right: 0)
+        }
+        secret.scrollIndicatorInsets = secret.contentInset
+        let selectedRange = secret.selectedRange
+        secret.scrollRangeToVisible(selectedRange)
     }
 }
 
